3e2793534b4319d43d0a33a4d22be488e67e0d39
mikelove <michaelisaiahlove@gmail.com>
2023-09-24 20:36:15 -0400

    more work

--- cd4-overlaps.R
+++ cd4-overlaps.R
@@ -12,7 +12,10 @@ library(tidyr)
 gene_tab <- se |>
   pivot_transcript() |>
   mutate(pb_ave_count = rowMeans(assay(se, "counts_scaled"))) |>
-  dplyr::select(gene=.feature, padj = P_sex_adjusted___cd4.naive, pb_ave_count) |>
+  dplyr::select(gene=.feature,
+                padj_sex = P_sex_adjusted___cd4.naive,
+                padj_sex_x_age = P_age_days.sex_adjusted___cd4.naive,
+                pb_ave_count) |>
   drop_na()
 
 ######################
@@ -113,15 +116,20 @@ load("intermediate.rda")
 library(plyranges)
 library(dplyr)
 
+fdr_threshold <- .05
+
 res <- g |>
-  filter(padj < .1) |>
+  filter(padj_sex < fdr_threshold | padj_sex_x_age < fdr_threshold) |>
   join_overlap_inner(gwas, maxgap=5e4) |>
   anchor_5p() |>
   mutate(width=1) |>
   mutate(tss_dist = (pos - start) * as.numeric(paste0(strand,1))) |>
   as_tibble() |>
-  dplyr::select(disease, chr=seqnames, de_gene=gene, gwas_gene, padj,
-         pb_ave_count, rsid, type, tss_dist) |>
+  dplyr::select(disease, chr=seqnames,
+                de_gene=gene, gwas_gene, # GWAS gene comes from GWAS pubs
+                padj_sex, padj_sex_x_age,
+                pb_ave_count, rsid,
+                type, tss_dist) |>
   arrange(disease, chr) 
 
 print(res, n=100)
@@ -135,27 +143,44 @@ res <- res |>
   mutate(disease = toupper(disease)) |>
   rename(SNP_type = type)
 
-res_distinct <- res |>
-  filter(de_gene == gwas_gene) |>
-  distinct(de_gene, .keep_all=TRUE) |>
-  mutate(x=0)
+cols <- c("MS" = "steelblue1",
+          "MS+RA" = "blue3",
+          "RA" = "magenta",
+          "RA+SLE" = "red",
+          "SLE" = "gold1",
+          "MS+SLE" = "forestgreen",
+          "MS+RA+SLE" = "black")
+
+res <- res |>
+  mutate(disease = factor(disease, levels=names(cols)))
 
 res |>
   filter(de_gene == gwas_gene) |>
+  filter(duplicated(rsid))
+
+print(res |> filter(de_gene == gwas_gene, tss_dist < 50e3) |> arrange(de_gene), n=20)
+
+res_distinct <- res |>
+  filter(de_gene == gwas_gene, tss_dist < 50e3) |>
+  distinct(de_gene, disease, .keep_all=TRUE) |>
+  group_by(de_gene, pb_ave_count) |>
+  summarize(disease = paste(disease, collapse="+")) |>  
+  mutate(x=0, SNP_type="intergenic") |>
+  mutate(disease = factor(disease, levels=names(cols)))
+
+# deal with a pleiotropic SNP
+res$disease[res$rsid == "rs4728142"] <- "MS+SLE"
+
+res |>
+  filter(de_gene == gwas_gene, tss_dist < 50e3) |>
   ggplot(aes(tss_dist, pb_ave_count, col=disease, shape=SNP_type)) +
   geom_point(size=3) +
-  geom_curve(aes(x = tss_dist, y = pb_ave_count,
-                 xend = 0, yend = pb_ave_count),
-             arrow = arrow(length = unit(.1, "inch")),
-             show.legend=FALSE) +
-  geom_text_repel(data = res_distinct,
-                  aes(x, pb_ave_count, label=de_gene),
-                  nudge_y = c(.25, 0, 0, 0, 0, 0),
-                  nudge_x = 5e3 * c(-1,-1,1,-1,-1,-1),
-                  min.segment.length = .2,
-                  point.padding = .5,
-                  box.padding = .8,
-                  show.legend = FALSE) +
-  xlab("distance from SNP to gene TSS") +
-  ylab("pseudobulk average count") +
-  scale_y_log10(limits=c(1,1e5))
+  geom_curve(aes(x = tss_dist, y = pb_ave_count, xend = 0, yend = pb_ave_count),
+             arrow = arrow(length = unit(.1, "inch")), show.legend=FALSE) +
+  geom_text_repel(data = res_distinct, aes(x, pb_ave_count, label=de_gene),
+                  nudge_x = -1e4, nudge_y = .8,
+                  min.segment.length = .2, point.padding = .5,
+                  box.padding = .8, show.legend = FALSE) +
+  xlab("distance from SNP to gene TSS") + ylab("pseudobulk average count") +
+  scale_y_log10(limits=c(1,1e5)) +
+  scale_color_manual(values=cols, limits=names(cols))
